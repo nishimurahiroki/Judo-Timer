d95be70f40ff4b24595b034574d132c083d9ad1e
Hiro <hirokinishimura44@gmail.com>
2025-12-18 15:36:44 +0900

    enable nextjs static export

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Allow external access for development
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000", "192.168.123.135:3000"],
-    },
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
